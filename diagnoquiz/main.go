e701cb55702015c6fbdd1fff7e85ccfca4b9aa07
Thinkpad <ginkobab@gmail.com>
2020-05-22 11:51:24 +0200

    implemented false levenshtein

--- diagnoquiz/main.go
+++ diagnoquiz/main.go
@@ -30,28 +30,23 @@ func QuizCriterioA() {
 
 }
 
-func CompareStrings(answer []string, solution []string) float32 {
-	var lenght int
-	for i, solWord := range solution {
-		for _, ansWord := range answer {
-
-			lenght = len(solWord)
-			if lenght < 3 {
-				continue
-			}
-
-			if strings.Contains(solWord, ansWord) {
-				solution[i] = "_"
-			} else {
-				solution[i] = string(solWord[:lenght-1])
-			}
-
-			fmt.Println(solWord, ansWord)
+func CompareStrings(answer, solution []string) (score float32) {
+	for _, ansWord := range answer {
+		var distances []int
+		for _, solWord := range solution {
+			fmt.Println("Call levensthein with words: ", ansWord, solWord)
+			fmt.Println("Levensthein distance: ", Levenshtein(ansWord, solWord))
+			distances = append(distances, Levenshtein(ansWord, solWord))
 		}
+		fmt.Println("distances: ", distances)
+		fmt.Println("min distances: ", minArray(distances))
+		score += minArray(distances)
 	}
 	fmt.Println(solution)
 
-	return 1.3
+	score /= lenStringArray(solution)
+
+	return score
 }
 
 func Input() []string {
@@ -73,4 +68,73 @@ func FormatString(s string) []string {
 
 	return sArray
 
+}
+
+func Levenshtein(s, t string) (distance int) {
+	//Todo: This is not actually levenshtein distance, need to apply dynamical programming
+	if len(s) == 0 {
+		return len(t)
+	}
+
+	if len(t) == 0 {
+		return len(s)
+	}
+
+	if t == s {
+		return 0
+	}
+
+	sRunes := []rune(s)
+	tRunes := []rune(t)
+	maxLenght := min(len(t), (len(s)))
+	for i := 0; i < maxLenght; i++ {
+		if sRunes[i] != tRunes[i] {
+			distance++
+		}
+
+	}
+	return distance
+}
+
+func argMin(s []int) float32 {
+	// Todo: check this works well
+	var index int
+	for i := range s {
+		if s[i] > s[index] {
+			continue
+		}
+
+		for l := range s {
+			if s[i] > s[l] {
+				index = l
+				continue
+			}
+		}
+	}
+	return float32(index)
+}
+
+func lenStringArray(s []string) float32 {
+	var lenght int
+	for _, str := range s {
+		lenght += len(str)
+	}
+
+	return float32(lenght)
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func minArray(s []int) float32 {
+	tmpMin := s[0]
+	for _, val := range s {
+		tmpMin = min(tmpMin, val)
+	}
+
+	return float32(tmpMin)
 }
