cd2c38f9ea6ff9bfd1efe27d36a2df4379476be6
Thinkpad <ginkobab@gmail.com>
2020-05-22 18:35:15 +0200

    levenshtein implemented

--- diagnoquiz/main.go
+++ diagnoquiz/main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
 	"math/rand"
-	"os"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -22,119 +20,120 @@ func main() {
 func QuizCriterioA() {
 	n := rand.Intn(numeroDisturbi)
 	disturbo := diagnosi.Diagnosi[n]
-	fmt.Println(disturbo.Criteri["A"])
+	disturbo.PrintCriterio("A")
+
 	answer := Input()
+	fmt.Printf("\nPrinting answer ->>%s<<-\n", answer)
+	if answer[0] == "" {
+		return
+	}
+	if answer[0] == "cognitivi" {
+		disturbo.Sintomi.PrintSintomiCognitivi(0)
+	}
+	if answer[0] == "comportamentali" {
+		disturbo.Sintomi.PrintSintomiComportamentali(0)
+	}
+	if answer[0] == "emotivi" {
+		disturbo.Sintomi.PrintSintomiEmotivi(0)
+	}
+	if answer[0] == "neurovegetativi" {
+		disturbo.Sintomi.PrintSintomiNeurovegetativi(0)
+	}
 
 	score := CompareStrings(answer, FormatString(disturbo.Nome))
-	fmt.Println(score)
+	ProcessScore(score, disturbo)
 
 }
 
-func CompareStrings(answer, solution []string) (score float32) {
-	for _, ansWord := range answer {
-		var distances []int
-		for _, solWord := range solution {
-			fmt.Println("Call levensthein with words: ", ansWord, solWord)
-			fmt.Println("Levensthein distance: ", Levenshtein(ansWord, solWord))
-			distances = append(distances, Levenshtein(ansWord, solWord))
-		}
-		fmt.Println("distances: ", distances)
-		fmt.Println("min distances: ", minArray(distances))
-		score += minArray(distances)
+func (d Disturbo) PrintCriterio(letter string) {
+	criterio := d.Criteri[letter]
+	fmt.Printf("%s%s%s\n", bold, criterio, colorReset)
+	if strings.Contains(criterio, "sintomi") {
+		// d.Sintomi.PrintSintomiCognitivi(0)
+		// d.Sintomi.PrintSintomiComportamentali(0)
+		// d.Sintomi.PrintSintomiEmotivi(0)
+		// d.Sintomi.PrintSintomiNeurovegetativi(0)
 	}
-	fmt.Println(solution)
-
-	score /= lenStringArray(solution)
-
-	return score
 }
 
-func Input() []string {
-	reader := bufio.NewReader(os.Stdin)
-	answer, err := reader.ReadString('\n')
-	if err != nil {
-		log.Fatal(err)
+func (s Sintomo) PrintSintomiCognitivi(num int) {
+	var index string
+	sKind := s.Cognitivi
+
+	fmt.Printf("\n[+] %sSintomi Cognitivi%s\n", bold, colorReset)
+
+	if num == 0 || num > len(sKind) {
+		num = len(sKind)
 	}
+	slice := rand.Perm(num)
+	for i := 0; i < len(sKind); i++ {
+		index = strconv.Itoa(slice[i] + 1)
+		fmt.Printf("[%s] %s\n", index, sKind[index])
+	}
+}
+func (s Sintomo) PrintSintomiComportamentali(num int) {
+	var index string
+	sKind := s.Comportamentali
 
-	answerArray := FormatString(answer)
-	return answerArray
+	fmt.Printf("\n[+] %sSintomi Comportamentali%s\n", bold, colorReset)
 
+	if num == 0 || num > len(sKind) {
+		num = len(sKind)
+	}
+	slice := rand.Perm(num)
+	for i := range slice {
+		index = strconv.Itoa(slice[i] + 1)
+		fmt.Printf("[%s] %s\n", index, sKind[index])
+	}
+}
+func (s Sintomo) PrintSintomiEmotivi(num int) {
+	var index string
+	sKind := s.Emotivi
+
+	fmt.Printf("\n[+] %sSintomi Emotivi%s\n", bold, colorReset)
+
+	if num == 0 || num > len(sKind) {
+		num = len(sKind)
+	}
+	slice := rand.Perm(num)
+	for i := range slice {
+		index = strconv.Itoa(slice[i] + 1)
+		fmt.Printf("[%s] %s\n", index, sKind[index])
+	}
+}
+func (s Sintomo) PrintSintomiNeurovegetativi(num int) {
+	var index string
+	sKind := s.Neurovegetativi
+
+	fmt.Printf("\n[+] %sSintomi Neurovegetativi%s\n", bold, colorReset)
+
+	if num == 0 || num > len(sKind) {
+		num = len(sKind)
+	}
+	slice := rand.Perm(num)
+	for i := range slice {
+		index = strconv.Itoa(slice[i] + 1)
+		fmt.Printf("[%s] %s\n", index, sKind[index])
+	}
 }
 
-func FormatString(s string) []string {
-	s = strings.ToLower(s)
-	s = strings.TrimSuffix(s, "\n")
-	sArray := strings.Split(s, " ")
-
-	return sArray
-
+func ProcessScore(score float32, disturbo Disturbo) {
+	if score < 0.1 {
+		fmt.Printf("%s%s%s\n", colorGreen, "[!] Perfetto!", colorReset)
+		return
+	} else if score < 0.3 {
+		fmt.Printf("%s%s%s\n", colorGreen, "[!] Sembra giusto, controlla la risposta!", colorReset)
+	} else if score < 0.5 {
+		fmt.Printf("%s%s%s\n", colorRed, "[!] Sembra sbagliato, controlla la risposta!", colorReset)
+	} else {
+		fmt.Printf("%s%s%s\n", colorRed, "[!] Nope", colorReset)
+	}
+	fmt.Printf("%s%s%s%s%s\n\n", "[>]", underlined, " La risposta era: ", disturbo.Nome, colorReset)
 }
 
-func Levenshtein(s, t string) (distance int) {
-	//Todo: This is not actually levenshtein distance, need to apply dynamical programming
-	if len(s) == 0 {
-		return len(t)
-	}
+const colorReset = "\033[0m"
+const colorRed = "\033[31m"
+const colorGreen = "\033[32m"
 
-	if len(t) == 0 {
-		return len(s)
-	}
-
-	if t == s {
-		return 0
-	}
-
-	sRunes := []rune(s)
-	tRunes := []rune(t)
-	maxLenght := min(len(t), (len(s)))
-	for i := 0; i < maxLenght; i++ {
-		if sRunes[i] != tRunes[i] {
-			distance++
-		}
-
-	}
-	return distance
-}
-
-func argMin(s []int) float32 {
-	// Todo: check this works well
-	var index int
-	for i := range s {
-		if s[i] > s[index] {
-			continue
-		}
-
-		for l := range s {
-			if s[i] > s[l] {
-				index = l
-				continue
-			}
-		}
-	}
-	return float32(index)
-}
-
-func lenStringArray(s []string) float32 {
-	var lenght int
-	for _, str := range s {
-		lenght += len(str)
-	}
-
-	return float32(lenght)
-}
-
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
-}
-
-func minArray(s []int) float32 {
-	tmpMin := s[0]
-	for _, val := range s {
-		tmpMin = min(tmpMin, val)
-	}
-
-	return float32(tmpMin)
-}
+const bold = "\033[1m"
+const underlined = "\033[4m"
